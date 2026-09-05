0658950d55258c7b40256aa0f454a272ec9764cc
nigamkumar3435-spec <nigamkumar3435@gmail.com>
2026-09-05 23:54:08 +0530

    Add files via upload

---
+++ Codes/AoC.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    float radius;
+
+    
+    printf("Enter the radius of the circle: ");
+    scanf("%f", &radius);
+
+    printf("Area of the circle is: %.2f\n", 3.14 * radius * radius);
+
+    return 0;
+}
+
